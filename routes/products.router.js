764e54835096c07b1b6bc321cc699bb4f6196b08
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2025-03-17 16:04:42 -0300

    add: progress class 16

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,51 +1,31 @@
 const express = require('express');
-const { faker } = require("@faker-js/faker");
+const ProductsService = require('./../services/product.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url()
-    });
-  }
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
-router.get('/filter', (req, res) => {
-  res.send('Filter');
-});
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   const { id } = req.params
-  if(id === '999'){
-    res.status(404).json({
-      message: 'Not Found'
-    });
-    return;
-  }
-  res.status(200).json([
-    {
-      id,
-      product: 'new product'
-    }
-  ])
+  const product = await service.findOne(id);
+  res.json(product);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   const body = req.body;
+  const newProduct = await service.create(body);
   res.status(201).json({
     message: 'Created',
-    data: body
+    data: newProduct
   });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   const { id } = req.params;
   const body = req.body;
   res.json({
@@ -55,21 +35,28 @@ router.put('/:id', (req, res) => {
   });
 });
 
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'Patched',
-    data: body,
-    id
-  });
+router.patch('/:id', async(req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json({
+      message: 'Patched',
+      data: product
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   const { id } = req.params;
+  const rta = await service.delete(id);
   res.json({
     message: 'Deleted',
-    id
+    id: rta.id
   });
 });
 
--- services/product.service.js
+++ services/product.service.js
@@ -1,5 +1,62 @@
-class ProductsSewrvice {
-  async getProducts() {
-    // fetch products from the server
+const { faker } = require("@faker-js/faker");
+class ProductsService {
+
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+  async generate(){
+    // console.log(faker.string)
+    const limit = 100;
+    for (let i = 0; i < limit; i++) {
+      this.products.push({
+        name: faker.commerce.productName(),
+        id: faker.string.uuid(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.url()
+      });
+    }
+  }
+  async create(data){
+    const newProduct = {
+      id: faker.string.uuid(),
+      ...data
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  async find(){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 2500);
+    });
+  }
+  async findOne(id){
+    return this.products.find(item => item.id === id);
+  }
+
+  async update(id, changes){
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw new Error('Product not found');
+    }
+    this.products[index] = {
+      ...this.products[index],
+      ...changes
+    };
+    return this.products[index];
+  }
+
+  async delete(id){
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
   }
 }
+
+module.exports = ProductsService;
