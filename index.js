ddd7b525f676897169db957dc7ed4826fcef551b
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2025-03-27 00:54:26 -0300

    add: progress class 24

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -7,6 +8,17 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const whitelist = ['http://localhost:8080', 'http://127.0.0.1:5500', 'https://myapp.cl'];
+const options = {
+  origin: function(origin, callback) {
+    if(whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
